0a838ee5c7c0d1b01288511ad4f4bbe3732ef3c0
mrkai1253 <asishshajithomas@gmail.com>
2020-01-27 07:40:52 +0530

    Initial commit

---
+++ test.js
@@ -0,0 +1,13 @@
+const tls = require('tls')
+const https = require('live-server-https');
+ 
+const server = tls.createServer(https, (socket) => {
+  console.log('server connected',
+              socket.authorized ? 'authorized' : 'unauthorized');
+  socket.write('welcome!\n');
+  socket.setEncoding('utf8');
+  socket.pipe(socket);
+});
+server.listen(8000, () => {
+  console.log('server bound');
+});
